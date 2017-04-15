ca793ff0cb7ed090840a3f5f7f60bb374507b684
Nakul97 <Nakul@Nakuls-MacBook-Pro.local>
2017-04-15 15:07:02 +0530

    SignUP

--- Pods/Target Support Files/Pods-Shopster/Pods-Shopster-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shopster : NSObject
-@end
-@implementation PodsDummy_Pods_Shopster
-@end
--- Pods/Target Support Files/SJFluidSegmentedControl/SJFluidSegmentedControl-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SJFluidSegmentedControl : NSObject
-@end
-@implementation PodsDummy_SJFluidSegmentedControl
-@end
